8c7528304b7e626e9a77de5a5187ee3c1103463c
Lenya <leonidivanov826@gmail.com>
2026-04-02 12:35:11 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Леонід Іванов on 02.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
